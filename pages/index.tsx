00b3945c2ad3d38c201022feb78b21fdc5532ec2
saintabomination <saint.abomination@protonmail.com>
2022-05-20 22:55:01 +0200

    Initial Commit

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Initial Commit!</h1>
+    </div>
+  );
+}
+
+export default Home;
