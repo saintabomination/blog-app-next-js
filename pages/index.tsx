af7537819beb95de61c1fa8203a7093e68c83431
saintabomination <saint.abomination@protonmail.com>
2022-05-20 23:04:46 +0200

    Home page layout base

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,34 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
+const posts = [
+  { title: 'Post 1', excerpt: 'Example text.' },
+  { title: 'Post 2', excerpt: 'Strawberries.' },
+];
+
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>Create Next App</title>
+        <title>CMS Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Initial Commit!</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="col-span-1 lg:col-span-8">
+          {posts.map(
+            (post, index) =>
+            <div key={index}>
+              {post.title}<br />
+              {post.excerpt}
+            </div>
+          )}
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative lg:sticky top-8">
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
